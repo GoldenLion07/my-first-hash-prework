ad6262bf36011b05d9c04addc7d83b9e8af1ce54
Scott M <gmat191@gmail.com>
2018-10-03 03:39:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
-def my_hash = {"name" => "Herman Melville", "book" => "Moby Dick", "occupation" => "Whaler"}
+def my_hash 
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+favorite_scotch =
   
 end
 
